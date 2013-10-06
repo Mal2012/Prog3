44858025f73b538803eb90e5ec87d5ce7c39f1d4
Mal2012 <malcolmkoegler+github@gmail.com>
2013-10-06 13:04:33 +0200

    ds

--- Prog3/main.c
+++ Prog3/main.c
@@ -4,11 +4,14 @@
  *  Created on: 06.10.2013
  *      Author: Malcolm
  */
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
+
 	a4();
 
-	puts("Hello Wurrrld");
-	return 0;
+
+	return EXIT_SUCCESS;
 }
 
